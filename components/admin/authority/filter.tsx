f0336b5c57b98dc0ef9499673ea4481e1859c031
pphetra <pphetra@gmail.com>
2022-06-02 15:30:54 +0700

    create admin authority list filter.

---
+++ components/admin/authority/filter.tsx
@@ -0,0 +1,47 @@
+import {
+  FilterTextInput,
+  ResetButton,
+  SearchButton,
+} from "components/widgets/filter";
+import Spinner from "components/widgets/spinner";
+import { observer } from "mobx-react";
+import React from "react";
+import { AdminAuthoorityListViewModel } from "./listViewModel";
+
+type Props = {
+  viewModel?: AdminAuthoorityListViewModel;
+};
+const Filter = ({ viewModel }: Props) => {
+  if (!viewModel) {
+    return <Spinner />;
+  }
+  return (
+    <div>
+      <FilterTextInput
+        type="text"
+        value={viewModel?.searchText}
+        placeholder="search"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          viewModel?.setSearchText(e.target.value)
+        }
+      />
+      <SearchButton
+        onClick={() => {
+          viewModel?.fetch();
+        }}
+      >
+        Search
+      </SearchButton>
+
+      <ResetButton
+        onClick={() => {
+          viewModel?.clearSearchText();
+        }}
+      >
+        Reset
+      </ResetButton>
+    </div>
+  );
+};
+
+export default observer(Filter);
--- components/admin/authority/list.tsx
+++ components/admin/authority/list.tsx
@@ -1,13 +1,9 @@
-import {
-  FilterTextInput,
-  ResetButton,
-  SearchButton,
-} from "components/widgets/filter";
 import Spinner from "components/widgets/spinner";
 import Table from "components/widgets/table";
 import { observer } from "mobx-react";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import Filter from "./filter";
 import { AdminAuthoorityListViewModel } from "./listViewModel";
 
 const AuthorityList = () => {
@@ -26,29 +22,7 @@ const AuthorityList = () => {
       <div className="mb-4">&gt;&gt; Authorities</div>
 
       <div className="flex items-center flex-wrap mb-4">
-        <FilterTextInput
-          type="text"
-          value={viewModel?.searchText}
-          placeholder="search"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            viewModel?.setSearchText(e.target.value)
-          }
-        />
-        <SearchButton
-          onClick={() => {
-            viewModel?.fetch();
-          }}
-        >
-          Search
-        </SearchButton>
-
-        <ResetButton
-          onClick={() => {
-            viewModel?.clearSearchText();
-          }}
-        >
-          Reset
-        </ResetButton>
+        <Filter viewModel={viewModel} />
       </div>
 
       <Table
