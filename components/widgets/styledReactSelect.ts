65227e1aa826eaa63ce3914b725d280cd0dbcc65
Wattana Chaidee <wchaidee@gmail.com>
2022-09-14 18:20:15 +0700

    styledReactSelect change any type to CSSObjectWithLabel

--- components/widgets/styledReactSelect.ts
+++ components/widgets/styledReactSelect.ts
@@ -1,10 +1,12 @@
+import { CSSObjectWithLabel } from "react-select";
+
 export const styledReactSelect = {
-  menu: (provided: any) => ({
+  menu: (provided: CSSObjectWithLabel) => ({
     ...provided,
     color: "rgb(37 99 235)",
   }),
   indicatorSeparator: () => ({}),
-  control: (base: any) => ({
+  control: (base: CSSObjectWithLabel) => ({
     ...base,
     boxShadow: "none",
   }),
