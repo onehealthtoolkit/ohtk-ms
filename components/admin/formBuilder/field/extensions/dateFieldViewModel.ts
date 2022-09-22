ab286e238dac3e8baa2a311fac3a04e844a9b9d8
Worawat Yao <worawatyao@gmail.com>
2022-09-22 17:34:20 +0700

    OSDAS-127 Support config withTime, separatedFields, backward/forward days offset in datefield

--- components/admin/formBuilder/field/extensions/dateFieldViewModel.ts
+++ components/admin/formBuilder/field/extensions/dateFieldViewModel.ts
@@ -2,18 +2,72 @@ import {
   AbstractDefinitionViewModel,
   Definition,
 } from "components/admin/formBuilder/shared";
-import { makeObservable } from "mobx";
+import { makeObservable, observable } from "mobx";
 
 export class DateFieldViewModel extends AbstractDefinitionViewModel {
+  withTime = false;
+  separatedFields = false;
+  backwardDaysOffset = "";
+  forwardDaysOffset = "";
+
   constructor() {
     super();
-    makeObservable(this, {});
+    makeObservable(this, {
+      withTime: observable,
+      separatedFields: observable,
+      backwardDaysOffset: observable,
+      forwardDaysOffset: observable,
+    });
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  parse(definition: Definition) {}
+  setWithTime(value: boolean) {
+    this.withTime = value;
+  }
+
+  setSeparatedFields(value: boolean) {
+    this.separatedFields = value;
+  }
+
+  setBackwardDaysOffset(value: string) {
+    const intVal = parseInt(value || "");
+    this.backwardDaysOffset = intVal >= 0 ? String(intVal) : "";
+  }
+
+  setForwarDaysOffset(value: string) {
+    const intVal = parseInt(value || "");
+    this.forwardDaysOffset = intVal >= 0 ? String(intVal) : "";
+  }
+
+  parse(definition: Definition) {
+    this.withTime = Boolean(definition.withTime);
+    this.separatedFields = Boolean(definition.separatedFields);
+    if (definition.backwardDaysOffset !== undefined) {
+      this.backwardDaysOffset = String(definition.backwardDaysOffset);
+    } else {
+      this.backwardDaysOffset = "";
+    }
+    if (definition.forwardDaysOffset !== undefined) {
+      this.forwardDaysOffset = String(definition.forwardDaysOffset);
+    } else {
+      this.forwardDaysOffset = "";
+    }
+  }
 
   toJson() {
-    return {};
+    const json: Definition = {
+      withTime: this.withTime,
+      separatedFields: this.separatedFields,
+    };
+
+    const backwardDaysOffsetNum = parseInt(this.backwardDaysOffset || "");
+    if (backwardDaysOffsetNum >= 0) {
+      json.backwardDaysOffset = backwardDaysOffsetNum;
+    }
+
+    const forwardDaysOffsetNum = parseInt(this.forwardDaysOffset || "");
+    if (forwardDaysOffsetNum >= 0) {
+      json.forwardDaysOffset = forwardDaysOffsetNum;
+    }
+    return json;
   }
 }
