bee4c848baeae3e2444277fe8f857a089e0c96fa
Wattana Chaidee <wchaidee@gmail.com>
2022-11-01 12:11:07 +0700

    OSDAS-99 missing breadcrumps  translate

--- pages/cases/index.tsx
+++ pages/cases/index.tsx
@@ -3,12 +3,15 @@ import Protect from "components/auth/protect";
 import Layout from "components/layout";
 import Breadcrumb from "components/layout/breadcrumb";
 import { NextPage } from "next";
+import { useTranslation } from "react-i18next";
 
 const CasesPage: NextPage = () => {
+  const { t } = useTranslation();
+
   return (
     <Protect>
       <Layout>
-        <Breadcrumb crumbs={[{ text: "Cases" }]} />
+        <Breadcrumb crumbs={[{ text: t("breadcrumb.cases", "Cases") }]} />
         <CaseList />
       </Layout>
     </Protect>
