a6518f64af4628fe1ce1069fa7ade8419240fabf
pphetra <pphetra@gmail.com>
2022-12-01 14:11:46 +0700

    make admin/AuthorityView dynamic loading component, because react-organizational-chart not working on server side rendering.

--- pages/admin/authorities/[id]/view.tsx
+++ pages/admin/authorities/[id]/view.tsx
@@ -1,11 +1,16 @@
 import { NextPage } from "next";
-import AuthorityView from "components/admin/authority/view";
 import Layout from "components/layout";
 import Protect from "components/auth/protect";
 import Breadcrumb from "components/layout/breadcrumb";
 import { useRouter } from "next/router";
 import Spinner from "components/widgets/spinner";
 import { useTranslation } from "react-i18next";
+import dynamic from "next/dynamic";
+
+const DynamicAuthorityView = dynamic(
+  () => import("components/admin/authority/view"),
+  { ssr: false }
+);
 
 const AdminAuthorityViewPage: NextPage = () => {
   const router = useRouter();
@@ -26,7 +31,7 @@ const AdminAuthorityViewPage: NextPage = () => {
             { text: t("breadcrumb.view", "View") },
           ]}
         />
-        <AuthorityView />
+        <DynamicAuthorityView />
       </Layout>
     </Protect>
   );
