d4063e7fe06ecf5b896829fb04f834a2baf06490
pphetra <pphetra@gmail.com>
2023-01-12 10:32:30 +0700

    support lao language.

--- i18n/index.js
+++ i18n/index.js
@@ -4,6 +4,7 @@ import { initReactI18next } from "react-i18next";
 import translationEn from "./translations.en.json";
 import translationTh from "./translations.th.json";
 import translationKm from "./translations.km.json";
+import translationLa from "./translations.la.json";
 
 export const resources = {
   en: {
@@ -14,6 +15,9 @@ export const resources = {
   },
   km: {
     translation: translationKm,
+  },
+  la: {
+    translation: translationLa,
   },
 };
 
